540b637a979cdd58325923290a056ba85c6b2c91
Martin <andy.pananar@gmail.com>
2018-04-17 23:14:06 -0500

    Header - Maquetado, vista para móvil

---
+++ resources/views/inicio.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.0/normalize.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/fontello.css">
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/toggle.js"></script>
+    <title>Pitahaya</title>
+</head>
+<body>
+    <main class="contenido">
+        <header class="cabecera">
+            <a href="#" class="logo">
+                <img src="img/logo_pitahaya.png" alt="Pitahaya">
+            </a>
+            <nav>
+                <div class="menu contenedor">
+                    <div>MENÚ</div>
+                    <div id="toggle-menu"><span class="icon-menu"></span></div>
+                </div>
+                <ul id="lista" class="lista">
+                    <li class="lista-item"><a href="#">Inicio</a></li>
+                    <li class="lista-item"><a href="#">Servicios</a></li>
+                    <li class="lista-item"><a href="#">Trabajos</a></li>
+                    <li class="lista-item"><a href="#">Nosotros</a></li>
+                    <li class="lista-item"><a href="#">Contacto</a></li>
+                    <li class="lista-item"><a href="#">Blog</a></li>
+                    <li class="lista-item"><a href="#">Entrar</a></li>
+                    <li class="lista-item"><a href="#">Crear Cuenta</a></li>
+                </ul>
+            </nav>
+            
+        </header>
+    </main>
+</body>
+</html>
