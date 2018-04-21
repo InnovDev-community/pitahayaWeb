80388ad29ed93b25b53c331f91bde561aef1b3e5
Martin <andy.pananar@gmail.com>
2018-04-21 10:11:30 -0500

    estructura del inicio

--- index.php
+++ index.php
@@ -1,60 +1,72 @@
-<?php
-
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
-
-define('LARAVEL_START', microtime(true));
-
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
-|
-*/
-
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.0/normalize.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/fontello.css">
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/toggle.js"></script>
+    <title>Pitahaya</title>
+</head>
+<body>
+    <main class="contenido">
+        <header class="cabecera">
+            <div class="cabecera-top">
+                <div class="item-cabecera-top">
+                    <p><span class="icon-mobile"></span>+51945584029</p>
+                </div>
+                <div class="item-cabecera-top">
+                    <p><span class="icon-mail"></span>pitahayaccorp@gmail.com</p>
+                </div>
+            </div>
+            <div class="cabecera-mid">
+                <a href="#" class="logo">
+                    <img src="img/logo_pitahaya.png" alt="Pitahaya">
+                </a>
+                <ul class="ingreso">
+                        <li class="ingreso-item"><a href="#">Entrar</a></li>
+                        <li class="ingreso-item"><a href="#">Crear Cuenta</a></li>
+                </ul>
+            </div>
+            <nav>
+                <div class="menu contenedor">
+                    <div>MENÚ</div>
+                    <div id="toggle-menu"><span class="icon-menu"></span></div>
+                </div>
+                <ul id="lista" class="lista">
+                    <li class="lista-item selected"><a href="#"><img class="imagen-inicio" src="img/nube.png">Inicio</a></li>
+                    <li class="lista-item"><a href="#">Servicios</a></li>
+                    <li class="lista-item"><a href="#">Trabajos</a></li>
+                    <li class="lista-item"><a href="#">Nosotros</a></li>
+                    <li class="lista-item"><a href="#">Contacto</a></li>
+                    <li class="lista-item"><a href="#">Blog</a></li>
+                    <li class="lista-item vista-movil"><a href="#">Entrar</a></li>
+                    <li class="lista-item vista-movil"><a href="#">Crear Cuenta</a></li>
+                </ul>
+            </nav>
+            
+        </header>
+        <section class="slider-contenedor">
+        </section>
+        <section class="sevicios-contenedor">
+        </section>
+        <section class="trabajos-contenedor">
+        </section>
+        <section class="blog-contenedor">
+        </section>
+        <section class="brief-basico-contenedor">
+        </section>
+        <section class="unetenos-contenedor">
+        </section>
+        <section class="contactanos">
+        </section>
+        <section class="pseudoFooter-contenedor">
+        </section>
+        <footer class="piedepagina">
+        </footer>
+    </main>
+</body>
+</html>
