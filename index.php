8a5f1ad8098fc645fadd7b9d8fd962d7a75a9c48
Martin <andy.pananar@gmail.com>
2018-04-23 12:46:11 -0500

    Normalize.css añadido

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.0/normalize.css">
+    <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/estilos.css">
     <link rel="stylesheet" href="css/fontello.css">
     <script src="js/jquery-3.3.1.min.js"></script>
